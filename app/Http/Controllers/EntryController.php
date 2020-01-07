3d59e6ad6fdc11b026ed70ce5774da47f545970c
Mikkycody <horluwatowbeey@gmail.com>
2020-01-07 13:05:47 +0100

    initial commit

---
+++ app/Http/Controllers/EntryController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EntryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $entries = Entry::paginate(15);
+
+        //return collection as a resource
+        return EntryResource::collection($entries);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'title' => 'required',
+            'body' => 'required',
+        ]);
+        $entry = new Entry;
+        $entry->user_id = $request->user()->id;
+        $entry->title = $request->title;
+        $entry->body = $request->body;
+        $entry->save();
+
+        return new EntryResource($entry);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(entry $entry)
+    {
+        return new EntryResource($entry);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Entry $entry)
+
+    {
+
+      // check if currently authenticated user is the owner of the entry
+
+      if ($request->user()->id !== $entry->user_id) {
+
+        return response()->json(['error' => 'You can only edit your own entries.'], 403);
+
+      }
+
+
+
+      $entry->update($request->only(['title', 'body']));
+
+
+
+      return new EntryResource($entry);
+    }
+
+  
+}
---
+++ app/Http/Resources/EntryResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class EntryResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        // return parent::toArray($request);
+ 
+        //transforms the resource into an array made up of the attributes to be converted to JSON
+        return [
+            'id'=> $this->id,
+            'title'=> $this->title,
+            'body'=>$this->body,
+            'created_at' => (string) $this->created_at,
+            'updated_at' => (string) $this->updated_at,
+            'user' => $this->user,
+        ];
+
+    }
+}
